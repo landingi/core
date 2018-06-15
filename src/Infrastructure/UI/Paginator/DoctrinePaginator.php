639a435d3cae35a8142107d49d57c467b7ce1fb0
bozenapietrzak <bozena@landingi.com>
2018-06-15 10:49:58 +0200

    Create DoctrinePaginator that decorate Orm\Tools\Paginator

---
+++ src/Infrastructure/UI/Paginator/DoctrinePaginator.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace Landingi\Shared\Infrastructure\UI\Paginator;
+
+use Doctrine\ORM\Tools\Pagination\Paginator as OrmPaginator;
+use Landingi\Shared\Infrastructure\UI\Paginator;
+use Landingi\Shared\Infrastructure\UI\Paginator\Query\QueryLimit;
+use Landingi\Shared\Infrastructure\UI\Paginator\Query\QueryOffset;
+
+class DoctrinePaginator implements Paginator
+{
+    /**
+     * @var OrmPaginator
+     */
+    private $paginator;
+    private $limit;
+    private $page;
+
+    public static function withOffset(OrmPaginator $paginator, Page $page, QueryLimit $limit)
+    {
+        $query = $paginator->getQuery();
+        $query->setFirstResult((new QueryOffset($page, $limit))->toNumber());
+        $query->setMaxResults($limit->toNumber());
+
+        return new self($paginator, $page, $limit);
+    }
+
+    public function __construct(OrmPaginator $paginator, Page $page, QueryLimit $limit)
+    {
+        $this->paginator = $paginator;
+        $this->page = $page;
+        $this->limit = $limit;
+    }
+
+    public function getItems() : array
+    {
+        return (array) $this->paginator->getIterator();
+    }
+
+    public function count() : int
+    {
+        return $this->paginator->count();
+    }
+
+    public function getPage() : Page
+    {
+        return $this->page;
+    }
+
+    public function getLimit() : int
+    {
+        return $this->limit->toNumber();
+    }
+}
+
