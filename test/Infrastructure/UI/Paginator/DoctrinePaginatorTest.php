d2f131c6d56e071067109db0824167e6a33cc588
bozenapietrzak <bozena@landingi.com>
2018-06-18 07:59:27 +0200

    Remove useless limit

--- test/Infrastructure/UI/Paginator/DoctrinePaginatorTest.php
+++ test/Infrastructure/UI/Paginator/DoctrinePaginatorTest.php
@@ -6,7 +6,6 @@ namespace Landingi\Shared\Infrastructure\UI\Paginator;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\Tools\Pagination\Paginator as OrmPaginator;
 use Landingi\Fake\FakeEntityManager;
-use Landingi\Shared\Infrastructure\UI\Paginator\Query\QueryLimit;
 use PHPUnit\Framework\TestCase;
 
 class DoctrinePaginatorTest extends TestCase
@@ -17,14 +16,10 @@ class DoctrinePaginatorTest extends TestCase
     /** @var Page */
     private $page;
 
-    /** @var QueryLimit */
-    private $limit;
-
     public function setUp()
     {
         $this->ormPaginator = $this->prophesize(OrmPaginator::class);
         $this->page = new Page(3);
-        $this->limit = new QueryLimit(10);
     }
 
     public function testGetItems()
