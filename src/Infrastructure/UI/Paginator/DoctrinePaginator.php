0a646affc9288f84a8b6fbbe86d3465a10e2872a
mnhck91 <boz3pie@gmail.com>
2018-06-18 08:26:32 +0200

    Create doctrine paginator (#6)

---
+++ src/Infrastructure/UI/Paginator/DoctrinePaginator.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Landingi\Shared\Infrastructure\UI\Paginator;
+
+use Doctrine\ORM\Tools\Pagination\Paginator as OrmPaginator;
+use Landingi\Shared\Infrastructure\UI\Paginator;
+
+class DoctrinePaginator implements Paginator
+{
+    private $paginator;
+    private $page;
+
+    public function __construct(OrmPaginator $paginator, Page $page)
+    {
+        $this->paginator = $paginator;
+        $this->page = $page;
+    }
+
+    public function getItems() : array
+    {
+        return (array) $this->paginator->getIterator();
+    }
+
+    public function count() : int
+    {
+        return $this->paginator->count();
+    }
+
+    public function getPage() : Page
+    {
+        return $this->page;
+    }
+
+    public function getLimit() : int
+    {
+        return $this->paginator->getQuery()->getMaxResults();
+    }
+}
---
+++ test/Infrastructure/UI/Paginator/DoctrinePaginatorTest.php
@@ -0,0 +1,74 @@
+<?php
+declare(strict_types=1);
+
+namespace Landingi\Shared\Infrastructure\UI\Paginator;
+
+use Doctrine\ORM\Query;
+use Doctrine\ORM\Tools\Pagination\Paginator as OrmPaginator;
+use Landingi\Fake\FakeEntityManager;
+use PHPUnit\Framework\TestCase;
+
+class DoctrinePaginatorTest extends TestCase
+{
+    /** @var OrmPaginator */
+    private $ormPaginator;
+
+    /** @var Page */
+    private $page;
+
+    public function setUp()
+    {
+        $this->ormPaginator = $this->prophesize(OrmPaginator::class);
+        $this->page = new Page(3);
+    }
+
+    public function testGetItems()
+    {
+        $this->ormPaginator->getIterator()->willReturn(['foo', 'bar']);
+        $this->ormPaginator->getQuery()->willReturn($this->buildQuery());
+
+        $doctrinePaginator = new DoctrinePaginator(
+            $this->ormPaginator->reveal(),
+            $this->page
+        );
+        self::assertCount(2, $doctrinePaginator->getItems());
+    }
+
+    public function testCount()
+    {
+        $this->ormPaginator->count()->willReturn(20);
+        $doctrinePaginator = new DoctrinePaginator(
+            $this->ormPaginator->reveal(),
+            $this->page
+        );
+        self::assertEquals(20, $doctrinePaginator->count());
+    }
+
+    public function testGetPage()
+    {
+        $doctrinePaginator = new DoctrinePaginator(
+            $this->ormPaginator->reveal(),
+            $this->page
+        );
+        self::assertEquals($this->page, $doctrinePaginator->getPage());
+    }
+
+    public function testGetLimit()
+    {
+        $ormQuery = $this->buildQuery();
+        $ormQuery->setMaxResults(10);
+        $this->ormPaginator->getQuery()->willReturn($ormQuery);
+        $doctrinePaginator = new DoctrinePaginator(
+            $this->ormPaginator->reveal(),
+            $this->page
+        );
+        self::assertEquals(10, $doctrinePaginator->getLimit());
+    }
+
+    private function buildQuery() : Query
+    {
+        return new Query(
+            new FakeEntityManager()
+        );
+    }
+}
